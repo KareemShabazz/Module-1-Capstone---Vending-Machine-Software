8e42bcf40bc03bb01e6fd8390f0233ac56c51c15
Dalton Lambert <lambertd757@gmail.com>
2022-10-07 09:37:00 -0400

    Commiting money provided display

---
+++ capstone/src/main/java/com/techelevator/Inventory.java
@@ -0,0 +1,6 @@
+package com.techelevator;
+
+public class Inventory { //creating a file in Scanner to retrieve item name from file
+    //print the item name and the stock
+    //Map would be adding the name and the stock
+}

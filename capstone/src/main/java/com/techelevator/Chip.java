5e775f58fc88522cb187f7365813e68cdfe83794
Dalton Lambert <lambertd757@gmail.com>
2022-10-07 15:50:11 -0400

    added chip

--- capstone/src/main/java/com/techelevator/Chip.java
+++ capstone/src/main/java/com/techelevator/Chip.java
@@ -1,20 +1,13 @@
 package com.techelevator;
 
 public class Chip extends VendingMachineItems{
-    private String itemCode;
-
-
     public Chip(String itemCode, String itemName, double itemPrice, String itemType) {
         super(itemCode, itemName, itemPrice, itemType);
-    }
 
+        Chip potatoCrisps = new Chip("B1","Potato Crisps", 3.05, "Chip");
+        Chip stackers = new Chip("B2","Stackers", 1.45, "Chip");
+        Chip grainWaves = new Chip("B3","Grain Waves", 2.75, "Chip");
+        Chip cloudPopcorn = new Chip("B4","Cloud Popcorn", 3.65, "Chip");
 
-   // public Chip(String code, String itemName, double itemPrice, String itemType){
-
-  //  }
-
-    @Override
-    public String getMessage(){
-        return "Crunch Crunch, Yum!";
     }
 }

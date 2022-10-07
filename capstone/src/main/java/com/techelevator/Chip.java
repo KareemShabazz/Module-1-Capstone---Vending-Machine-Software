28c354c309b7c26e1b57c17cb156ee30837a3147
Kareem Shabazz <shabazz1994@gmail.com>
2022-10-07 15:32:58 -0400

    update

--- capstone/src/main/java/com/techelevator/Chip.java
+++ capstone/src/main/java/com/techelevator/Chip.java
@@ -1,12 +1,13 @@
 package com.techelevator;
 
-public class Chips extends VendingMachineItems{
+public class Chip extends VendingMachineItems{
     private String itemCode;
 
 
-    public Chips(String itemCode, String itemName, String itemType, int itemStock, double itemPrice, String itemSlot) {
-        super(itemCode, itemName, itemType, itemStock, itemPrice, itemSlot);
+    public Chip(String itemCode, String itemName, double itemPrice, String itemType) {
+        super(itemCode, itemName, itemPrice, itemType);
     }
+
 
    // public Chip(String code, String itemName, double itemPrice, String itemType){
 
--- capstone/src/main/java/com/techelevator/Gum.java
+++ capstone/src/main/java/com/techelevator/Gum.java
@@ -2,9 +2,18 @@ package com.techelevator;
 
 public class Gum extends VendingMachineItems{
 
-    public Gum(String itemCode, String itemName, String itemType, int itemStock, double itemPrice, String itemSlot) {
-        super(itemCode, itemName, itemType, itemStock, itemPrice, itemSlot);
+    public Gum(String itemCode, String itemName, double itemPrice, String itemType) {
+        super(itemCode, itemName, itemPrice, itemType);
+
+        Gum uChews = new Gum("D1", "U-Chews", 0.85, "Gum");
+        Gum littleLeague = new Gum("D2", "Little League", 0.95, "Gum");
+        Gum chiclets = new Gum("D3", "Chiclets", 0.75, "Gum");
+        Gum triplemint = new Gum("D4", "Triplemint", 0.75, "Gum");
+
+
     }
+
+
 
     @Override
     public String getMessage(){
